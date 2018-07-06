d4b85fbf7869371b1feb682d2d2b9800507dc172
adriandurran <adriandurran@users.noreply.github.com>
2018-07-06 15:03:26 +0100

    refactor: remove redux-form

--- client/src/actions/printers.js
+++ client/src/actions/printers.js
@@ -6,9 +6,7 @@ import {
   FETCH_PRINTER,
   FETCH_PRINTER_JOBS,
   FETCH_JOBS_SUCCESS,
-  DELETE_PRINTER_JOB,
-  FETCH_PRINTER_TOKEN,
-  FETCH_TOKEN_SUCCESS
+  DELETE_PRINTER_JOB
 } from './types';
 
 export const fetchPrinters = () => async dispatch => {
@@ -49,51 +47,6 @@ function jobsSuccess(jobsList) {
   };
 }
 
-export const fetchPrinterToken = () => async dispatch => {
-  dispatch(fetchingPrinterToken());
-  const res = await axios.get(`/api/printer/token`);
-  console.log(res.data);
-  dispatch(tokenSuccess(res.data));
-};
-
-// this is for dev only delete in production
-export const createPrinterToken = () => async dispatch => {
-  let tokenO = {
-    tokenname: 'test_token',
-    token: 'cassaf-cmhfwejwjqixjxakjdbjkabTTTT'
-  };
-  const res = await axios.post('/api/printer/token', tokenO);
-  // console.log(res.data);
-  dispatch(tokenSuccess(res.data));
-};
-
-export const rotatePrinterToken = gcpAdmin => async dispatch => {
-  const res = await axios.post('/api/printer/token/rotate', gcpAdmin);
-  if (res.status === 200) {
-    // check contents of data......while we dev
-    console.log(res.data);
-    // if it is what I think it will be then
-    //  dispatch(tokenSuccess(res.data));
-    // also need to clear the form
-  } else {
-    // need to handle the error in some way
-    console.log(res.status, res.data);
-  }
-};
-
-function tokenSuccess(tokenDetails) {
-  return {
-    type: FETCH_TOKEN_SUCCESS,
-    payload: tokenDetails
-  };
-}
-
-function fetchingPrinterToken() {
-  return {
-    type: FETCH_PRINTER_TOKEN
-  };
-}
-
 export const fetchPrinter = printerId => async dispatch => {
   const res = await axios.get(`/api/google/printer/${printerId}`);
   dispatch({ type: FETCH_PRINTER, payload: res.data });
@@ -107,11 +60,3 @@ export const deletePrinterJob = jobid => async dispatch => {
     // need to convey some sort of error
   }
 };
-
-// will need to do the following :
-// get the printer jobs but use the pc token....
-// delete the printer jobs  but use the pc token....
-// pc token never stored on the client???? Or only stored on the client for the session
-// server/client fetch pc token from the db....check age of token....if token is
-// older than an agreed time.....before any deleting can be done...refresh the token
-// to refresh the pc token......it will need the correct credentials......where/what???
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,19 +1,15 @@
 import { combineReducers } from 'redux';
-import { reducer as reduxForm } from 'redux-form';
 
 import authReducer from './authReducer';
 import printerReducer from './printerReducer';
 import printerDetailReducer from './printerDetailReducer';
 import printerJobsReducer from './printerJobsReducer';
-import printerTokenReducer from './printerTokenReducer';
 
 const reducer = combineReducers({
   auth: authReducer,
   printers: printerReducer,
   printer: printerDetailReducer,
-  jobs: printerJobsReducer,
-  token: printerTokenReducer,
-  form: reduxForm
+  jobs: printerJobsReducer
 });
 
 export default reducer;
